562f9ad1e515848328b1ecff8b03c6d640300486
MShahbazh <msh23904t@gmail.com>
2026-05-12 11:07:29 +0500

    Added

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,14 @@
 
 const teacher=JSON.parse(localStorage.getItem('teacher'));
+const student=JSON.parse(localStorage.getItem('student'));
 if (teacher) {
-    document.getElementById('login_button').textContent='ENTER'
-    document.getElementById('login_button').href='/public/teacher.html'
+  document.getElementById('login_button').textContent='ENTER'
+  document.getElementById('login_button').href='/public/teacher.html'
+}
+else if(student){
+  
+  document.getElementById('login_button').textContent='ENTER'
+  document.getElementById('login_button').href='/public/student.html'
   }
   else{
     document.getElementById('login_button').textContent='LOGIN'
